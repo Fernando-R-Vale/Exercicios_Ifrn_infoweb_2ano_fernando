0cb17059b967397bed6108c9056f90146a3bbb85
Fernando Rego do Vale <121295486+Fernando-R-Vale@users.noreply.github.com>
2023-04-10 11:00:20 -0300

    Create Ex03.py

---
+++ Lista 02/Ex03.py
@@ -0,0 +1,3 @@
+N1 = int(input("Digite a nota do primeiro bimestre da disciplina:"))
+N2 = int(input("Digite a nota do segundo bimestre da disciplina:"))
+print(f"Média parcial = {(N1 * 2 + N2 * 3)/5:.0f}")
