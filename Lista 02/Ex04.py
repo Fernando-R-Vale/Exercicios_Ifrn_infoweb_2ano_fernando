c651729e32b5b558d18bd3ce6eb96b1d090af7b6
Fernando Rego do Vale <121295486+Fernando-R-Vale@users.noreply.github.com>
2023-04-10 11:05:40 -0300

    Create Ex04.py

---
+++ Lista 02/Ex04.py
@@ -0,0 +1,3 @@
+Base = int(input(Digite a base e a altura do retângulo))
+alt = int(input())
+print(f'Área = {Base*alt} - Perímetro = {Base * 2 + alt * 2} - Diagonal = {(Base**2 + alt**2)** (1/2)}')
